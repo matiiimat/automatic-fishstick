89134a6b522fba919df14ce0440ce4700aa38cf4
matiiimat <109181275+matiiimat@users.noreply.github.com>
2023-04-05 23:13:59 +0200

    v2.2

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const ethereumButton = document.querySelector('.enableEthereumButton');
+const showAccount = document.querySelector('.showAccount');
 
 ethereumButton.addEventListener('click', () => {
-    //Will Start the metamask extension
-    ethereum.request({ method: 'eth_requestAccounts' });
-})
+    getAccount();
+});
+
+async function getAccount() {
+    const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+    const account = accounts[0];
+    showAccount.innerHTML = account;
+}
