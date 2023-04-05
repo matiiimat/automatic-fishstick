b72736493467e77d3ab3e2be51b112208623d054
matiiimat <109181275+matiiimat@users.noreply.github.com>
2023-04-05 23:30:01 +0200

    cleaning up v2

--- app.js
+++
@@ -1,9 +0,0 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/web3/1.7.4-rc.1/web3.min.js"></script><script>/* To connect using MetaMask */
-async function connect() { if (window.ethereum) {
- 
-  await window.ethereum.request({ method: "eth_requestAccounts" });
-  window.web3 = new Web3(window.ethereum);
-  
- } else {
-  console.log("No wallet");
- }}</script>
