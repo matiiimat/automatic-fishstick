ca5cd8a1a1ac039f83e15dfa475e7cc1cb87d38a
matiiimat <109181275+matiiimat@users.noreply.github.com>
2023-04-05 23:21:03 +0200

    v3

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
-const ethereumButton = document.querySelector('.enableEthereumButton');
-const showAccount = document.querySelector('.showAccount');
-
-ethereumButton.addEventListener('click', () => {
-    getAccount();
-});
-
-async function getAccount() {
-    const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
-    const account = accounts[0];
-    showAccount.innerHTML = account;
-}
+<script src="https://cdnjs.cloudflare.com/ajax/libs/web3/1.7.4-rc.1/web3.min.js"></script><script>/* To connect using MetaMask */
+async function connect() { if (window.ethereum) {
+ 
+  await window.ethereum.request({ method: "eth_requestAccounts" });
+  window.web3 = new Web3(window.ethereum);
+  
+ } else {
+  console.log("No wallet");
+ }}</script>
