e38b7dc55d784a0dad121cf55148963ffb14310a
matiiimat <109181275+matiiimat@users.noreply.github.com>
2023-04-05 23:11:35 +0200

    v2.1

--- app.js
+++ app.js
@@ -1,21 +1,6 @@
-document.getElementById('connect-btn').addEventListener('click', connectMetaMask);
+const ethereumButton = document.querySelector('.enableEthereumButton');
 
-async function connectMetaMask() {
-    // Check if MetaMask is installed
-    if (typeof window.ethereum !== 'undefined') {
-        const web3 = new Web3(window.ethereum);
-
-        try {
-            // Request account access
-            const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-
-            // Successfully connected to MetaMask
-            console.log('Connected to MetaMask:', accounts[0]);
-        } catch (error) {
-            // User denied account access
-            console.error('User denied account access:', error);
-        }
-    } else {
-        alert('MetaMask is not installed. Please consider installing it: https://metamask.io/download.html');
-    }
-}
+ethereumButton.addEventListener('click', () => {
+    //Will Start the metamask extension
+    ethereum.request({ method: 'eth_requestAccounts' });
+})
